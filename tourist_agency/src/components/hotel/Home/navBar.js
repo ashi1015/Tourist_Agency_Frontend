57edd43a966b452f725a2385cd3534ac499007c0
Ashi1015 <56013090+ashi1015@users.noreply.github.com>
2021-09-08 13:20:50 +0530

    Hotel management implementation

---
+++ tourist_agency/src/components/hotel/Home/navBar.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+
+export default class NavBar extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+
+        //     <div data-testid="nav">
+        //     <nav className="navbar navbar-expand navbar-dark bg-dark">
+        //         <ul className="nav nav-pills">
+        //             <li className="nav-item">
+        //                 <a className="nav-link active" aria-current="page" href="/tutorials">Travel Agency</a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a data-testid="DashboardlinkTest" className="nav-link active" aria-current="page" href="/">Dashboard</a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a data-testid="ResearchlinkTest" className="nav-link active" href="/Researches">Researches</a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a data-testid="WorkshoplinkTest" className="nav-link active" href="/Workshops">Workshops</a>
+        //             </li>
+        //         </ul>
+        //     </nav>
+        // </div>
+
+
+// <div>
+// <nav className="navbar navbar-expand navbar-dark bg-dark">
+//   <a href="/tutorials" className="navbar-brand">
+//     Travel Agency
+//   </a>
+//   <div className="navbar-nav mr-auto">
+//     <li className="nav-item">
+//       <Link to={"/touristList"} className="nav-link">
+//         Hotels List
+//       </Link>
+//     </li>
+//     <li className="nav-item">
+//       <Link to={"/add"} className="nav-link">
+//         Add
+//       </Link>
+//     </li>
+//     <li className="nav-item">
+//       <Link to={"/hotels"} className="nav-link">
+//         Hotels
+//       </Link>
+//     </li>
+//     <li className="nav-item">
+//       <Link to={"/tourists"} className="nav-link">
+//         Tourists
+//       </Link>
+//     </li>
+//     <li className="nav-item">
+//       <Link to={"/HotelDashboard"} className="nav-link">
+//         Hotels Dashboard
+//       </Link>
+//     </li>
+//     <li className="nav-item">
+//       <Link to={"/contactUs"} className="nav-link">
+//         Contact Us
+//       </Link>
+//     </li>
+//   </div>
+// </nav>
+
+// <div className="container mt-3"></div>
+// </div>
+
+<div></div>
+
+
+        )
+    }
+}
---
+++ tourist_agency/src/components/hotel/tourist_view.component.js
@@ -0,0 +1,274 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+import { Link } from "react-router-dom";
+import Pagination from "@material-ui/lab/Pagination";
+import destination1 from '../images/destination1.jpg'
+
+export default class TouristView extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+        this.retrieveTutorials = this.retrieveTutorials.bind(this);
+        this.refreshList = this.refreshList.bind(this);
+        this.setActiveTutorial = this.setActiveTutorial.bind(this);
+        this.removeAllTutorials = this.removeAllTutorials.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.handlePageSizeChange = this.handlePageSizeChange.bind(this);
+
+        this.state = {
+            tutorials: [],
+            currentTutorial: null,
+            currentIndex: -1,
+            searchTitle: "",
+
+            page: 1,
+            count: 0,
+            pageSize: 3,
+        };
+
+        this.pageSizes = [3, 6, 9];
+    }
+
+    componentDidMount() {
+        this.retrieveTutorials();
+    }
+
+    onChangeSearchTitle(e) {
+        const searchTitle = e.target.value;
+
+        this.setState({
+            searchTitle: searchTitle,
+        });
+    }
+
+    getRequestParams(searchTitle, page, pageSize) {
+        let params = {};
+
+        if (searchTitle) {
+            params["hotelName"] = searchTitle;
+        }
+
+        return params;
+    }
+
+    retrieveTutorials() {
+        const { searchTitle, page, pageSize } = this.state;
+        const params = this.getRequestParams(searchTitle, page, pageSize);
+
+        TutorialDataService.getAll(params)
+            .then((response) => {
+                const { tutorials, totalPages } = response.data;
+
+                this.setState({
+                    tutorials: tutorials,
+                    count: totalPages,
+                });
+                console.log(response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
+    refreshList() {
+        this.retrieveTutorials();
+        this.setState({
+            currentTutorial: null,
+           
+        });
+    }
+
+    setActiveTutorial(tutorial, index) {
+        this.setState({
+            currentTutorial: tutorial,
+         
+        });
+    }
+
+    removeAllTutorials() {
+        TutorialDataService.deleteAll()
+            .then((response) => {
+                console.log(response.data);
+                this.refreshList();
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
+    handlePageChange(event, value) {
+        this.setState(
+            {
+                page: value,
+            },
+            () => {
+                this.retrieveTutorials();
+            }
+        );
+    }
+
+    handlePageSizeChange(event) {
+        this.setState(
+            {
+                pageSize: event.target.value,
+                page: 1
+            },
+            () => {
+                this.retrieveTutorials();
+            }
+        );
+    }
+
+    render() {
+        const {
+            searchTitle,
+            tutorials,
+            currentTutorial,
+            currentIndex,
+            page,
+            count,
+            pageSize,
+        } = this.state;
+
+        return (
+            <div >
+                <div className="col-md-12">
+
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by title"
+                            value={searchTitle}
+                            onChange={this.onChangeSearchTitle} />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.retrieveTutorials} >
+                                Search
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-12">
+
+                    <h4>Tourist List</h4>
+
+
+                </div><br/><br/>
+
+                <div className="col-md-12">
+
+                    {tutorials &&
+                    tutorials.map((tutorial, index) => (
+
+                    <div className="card" style={{display: 'flex', flexDirection: 'row'}} style={{width: "18rem"}}>
+                        <img className="card-img-top" src="../images/h1.jpg" alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className={ "card-title " + (index === currentIndex ? "active" : "")}
+                                onClick={() => this.setActiveTutorial(tutorial, index)}
+                                key={index}>
+                                {tutorial.hotelName}
+                            </h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                            the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+
+                        </div>
+
+                    </div>  ))} <br/><br/>
+
+
+                </div>
+
+
+                <div className="col-md-12">
+
+                    <div className="startP">
+
+                        {"Items per Page: "}
+                        <select className="select" onChange={this.handlePageSizeChange} value={pageSize}>
+                            {this.pageSizes.map((size) => (
+                                <option key={size} value={size}>
+                                    {size}
+                                </option>
+                            ))}
+                        </select>
+
+                        <Pagination
+                            className="pagination"
+                            count={count}
+                            page={page}
+                            siblingCount={1}
+                            boundaryCount={1}
+                            variant="outlined"
+                            shape="rounded"
+                            onChange={this.handlePageChange}
+                        />
+                    </div>
+
+                </div>
+
+
+                <br/><br/><br/><br/>
+                <div className="col-md-12">
+                    {currentTutorial ? (
+                        <div>
+                            <div>
+                                <label><strong>Title :</strong></label>{" "}
+                                <label style={{textIndent: "100px"}}>{currentTutorial.hotelName}</label>
+                            </div>
+                            <div>
+                                <label><strong>Description :</strong></label>{" "}
+                                <label style={{textIndent: "50px"}}>{currentTutorial.description}</label>
+                            </div>
+                            <div>
+                                <label><strong>City :</strong></label>{" "}
+                                {currentTutorial.city}
+                                {/*{currentTutorial.published ? "Published" : "Pending"}*/}
+                            </div>
+                            <div>
+                                <label><strong>Coordinates :</strong></label>{" "}
+                                <label>{currentTutorial.coordinates}</label>
+                            </div>
+                            <div>
+                                <label><strong>Area (ha) :</strong></label>{" "}
+                                {currentTutorial.area}
+                            </div>
+                            <div>
+                                <label><strong>Altitude :</strong></label>{" "}
+                                {currentTutorial.altitude}
+                            </div>
+                            <div>
+                                <label><strong>Temperature :</strong></label>{" "}
+                                {currentTutorial.temperature}
+                            </div>
+                            <div>
+                                <label><strong>Contact :</strong></label>{" "}
+                                {currentTutorial.contact}
+                            </div>
+                            <div>
+                                <label><strong>ID / Visa Requirement :</strong></label>{" "}
+                                {currentTutorial.visaRequirement}
+                            </div>
+                            <Link
+                                to={"/tutorials/" + currentTutorial.id}
+                                className="badge badge-warning">
+                                Edit
+                            </Link> <br/><br/><br/><br/>
+                        </div>
+
+                    ) : (
+                        <div>
+                            <br />
+                            <p>Please click on a Tutorial...</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
