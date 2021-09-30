bea21098da129be5f3a98adccbaa8f5519298f34
ashi1015 <ayeshpramo@gmail.com>
2021-09-30 06:51:50 +0530

    modifying admin dashboard and adding vehicle image

--- src/components/MainComponents/AdminDashboard.js
+++ src/components/MainComponents/AdminDashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import add from '../../images/add.png';
-import report from '../../images/report.png'
-import update from '../../images/update.png'
-import view from '../../images/view2.png'
+import hotel from '../../images/h1.jpg';
+import destination from '../../images/destination1.jpg'
+import vehicle from '../../images/car.jpg'
+import user from '../../images/man1.jpg'
+import guide from '../../images/lady.jpg'
+import expense from '../../images/report.png'
 
 
 export default class AdminDashboard extends Component {
@@ -32,7 +34,7 @@ export default class AdminDashboard extends Component {
                         <div className="card text-white bg-info mb-4" style={{width: "15rem"}}>
                             <div className="card-body">
                                 <Link to={"/add"}>
-                                    <img className="card-img-top" src={add} alt="Card image cap" />
+                                    <img className="card-img-top" src={hotel} alt="Card image cap" />
                                     <p className="card-text">Hotel Management</p>
                                 </Link>
                             </div>
@@ -42,7 +44,7 @@ export default class AdminDashboard extends Component {
                         <div className="card text-white bg-light mb-3" style={{ width: "15rem"}}>
                             <div className="card-body">
                                 <Link to={"/hotelList"}>
-                                    <img className="card-img-top" src={update} alt="Card image cap" />
+                                    <img className="card-img-top" src={destination} alt="Card image cap" />
                                     <p className="card-text">Destination Management</p>
                                 </Link>
                             </div>
@@ -52,39 +54,39 @@ export default class AdminDashboard extends Component {
                         <div className="card text-white bg-secondary mb-3" style={{width: "15rem"}} >
                             <div className="card-body">
                                 <Link to={"/individualDestinationDetails"}>
-                                    <img className="card-img-top" src={view} alt="Card image cap" />
+                                    <img className="card-img-top" src={vehicle} alt="Card image cap" />
                                     <p className="card-text">Vehicle Management</p>
                                 </Link>
                             </div>
                         </div>
                     </div>
-                    <div className="column" >
+                    <div className="column" style={{height: "15rem"}}>
                         <div className="card text-white bg-light mb-3" style={{width: "15rem"}}>
                             <div className="card-body">
                                 <Link to={"/HotelDashbaord/Report"}>
-                                    <img className="card-img-top" src={report} alt="Card image cap" />
+                                    <img className="card-img-top" src={guide} alt="Card image cap" style={{height: "170px"}}/>
                                     <p className="card-text">Tourist Guide Management</p>
                                 </Link>
                             </div>
                         </div>
                     </div>
 
-                    <div className="column">
+                    <div className="column" style={{height: "15rem"}}>
                         <div className="card text-white bg-light mb-3" style={{width: "15rem"}}>
                             <div className="card-body">
                                 <Link to={"/HotelDashbaord/Report"}>
-                                    <img className="card-img-top" src={report} alt="Card image cap" />
+                                    <img className="card-img-top" src={user} alt="Card image cap" style={{height: "170px"}}/>
                                     <p className="card-text">User Profile Management</p>
                                 </Link>
                             </div>
                         </div>
                     </div>
 
-                    <div className="column">
+                    <div className="column" style={{height: "17.5rem"}}>
                         <div className="card text-white bg-light mb-3" style={{width: "15rem"}}>
                             <div className="card-body">
                                 <Link to={"/HotelDashbaord/Report"}>
-                                    <img className="card-img-top" src={report} alt="Card image cap" />
+                                    <img className="card-img-top" src={expense} alt="Card image cap" style={{height: "170px"}} />
                                     <p className="card-text">Expenses Management</p>
                                 </Link>
                             </div>
